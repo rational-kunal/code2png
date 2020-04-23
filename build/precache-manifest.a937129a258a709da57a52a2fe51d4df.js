788dd498dd56cb07e93c54deb4582a6b5e62fbbd
kunal <kunalgk1999@gmail.com>
2020-04-23 20:19:01 +0530

    build

---
+++ build/precache-manifest.a937129a258a709da57a52a2fe51d4df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f25f30be70da78b4a15654c573b389e",
+    "url": "/code2png/index.html"
+  },
+  {
+    "revision": "a2a1b36a8b4722ce06f0",
+    "url": "/code2png/static/css/main.03d1f16e.chunk.css"
+  },
+  {
+    "revision": "aa20fba2dc197bbaf8ec",
+    "url": "/code2png/static/js/2.3e81f4bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/code2png/static/js/2.3e81f4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a1b36a8b4722ce06f0",
+    "url": "/code2png/static/js/main.ae9c4809.chunk.js"
+  },
+  {
+    "revision": "caf949d59198d9439703",
+    "url": "/code2png/static/js/runtime-main.301980cb.js"
+  }
+]);
